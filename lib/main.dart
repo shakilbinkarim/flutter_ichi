f5287b5c3d3841f0b440cb9e5201bcac1e704c49
shakilbinkarim <karimshakilbin@gmail.com>
2018-09-01 23:58:35 +0900

    今日の最後のcommit。色がチェジした。

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        canvasColor: Colors.white70,
+        accentColor: Colors.indigo,
+      ),
       home: Scaffold(
           appBar: AppBar(
             title: Text('簡単リスト'),
           ),
-          body: ProductManager()),
+          body: ProductManager('食戟のソーマ')),
     );
   }
 }
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,12 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:ichi/products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct);
+
   @override
   _ProductManagerState createState() => _ProductManagerState();
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['食品のテスター'];
+  List<String> _products = [];
+
+  @override
+  void initState() { // called before the build method is called
+    super.initState();
+    // the "widget" keyword gives the class access to properties of it's widgets
+    _products.add(widget.startingProduct);
+  }
+
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +33,7 @@ class _ProductManagerState extends State<ProductManager> {
         Container(
           margin: EdgeInsets.all(10.0),
           child: RaisedButton(
+            color: Theme.of(context).primaryColor,
             onPressed: () {
               setState(() {
                 _products.add('新しい食品');
